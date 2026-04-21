e9e211d8ee65f378c766248b22a8a7856d5645b5
David <meeddry@gmail.com>
2026-04-22 01:23:39 +0300

    sol_4_with_pytest

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -5,24 +5,29 @@ from app.restore_names import restore_names
 @pytest.mark.parametrize(
     "users, expected",
     [
-        # first_name = None
         (
             [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}],
             "Jack",
         ),
-        # first_name відсутній
         (
             [{"last_name": "Adams", "full_name": "Mike Adams"}],
             "Mike",
         ),
-        # first_name вже є → не змінюється
         (
-            [{"first_name": "John", "last_name": "Doe", "full_name": "Mike Doe"}],
+            [
+                {
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "full_name": "Mike Doe",
+                }
+            ],
             "John",
         ),
     ],
 )
-def test_restore_names(users, expected) -> None:
+def test_restore_names(
+    users: list[dict], expected: str
+) -> None:
     restore_names(users)
     assert users[0]["first_name"] == expected
 
